b35ca9bd5d30708bd7b36123a48751ff229f6544
Chronos Ouroboros <doom2.fan@gmail.com>
2018-07-02 03:55:18 -0300

    Derp.

---
+++ GZDoomIDE/Windows/SettingsWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GZDoomIDE.Windows {
+    public partial class SettingsWindow : Form {
+        public SettingsWindow () {
+            InitializeComponent ();
+        }
+
+        private void ButtonOK_Click (object sender, EventArgs e) {
+
+        }
+    }
+}
